c70cec514e8e7d22f9556961fcec69afc0a463f2
esdrasgc <esdrasgcdev@gmail.com>
2024-04-14 14:13:50 -0300

    colisao com o player bugada

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -5,6 +5,9 @@ public class Animal : MonoBehaviour
     public float speed = 5.0f;  // Velocidade padrão de movimento
     public Transform playerTransform;  // Referência para o transform do jogador
 
+    public float bounceForce = 150.0f;
+
+
     protected virtual void Start()     // Esse virtual serve pra que as classes filhas possam sobrescrever esse método
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player");
@@ -29,7 +32,16 @@ public class Animal : MonoBehaviour
     void MoveTowardsPlayer()
     {
         Vector3 direction = (playerTransform.position - transform.position).normalized;
-        direction.y = 0;
+        // direction.y = 0;
         transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.AddExplosionForce(bounceForce, transform.position, 1.0f, 0.0f, ForceMode.Impulse);
+        }
     }
 }
